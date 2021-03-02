c0989d4b101c44a86cbc09ba278e093e5c2e67ee
RenasSitdikov <renas.sitdikov@flatstack.com>
2021-03-02 19:52:53 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './App.css';
+import Examples from './examples';
+
+const initialList = [
+    { name: 'Скачать и попробовать установить ESLnt и Prettier' },
+];
+
+function App() {
+    const [list, setList] = useState(initialList);
+    const addToList = (newElement) => setList([ ...list, { name: newElement }]);
+    const exampleProps = {
+        size: 5,
+        error: false,
+        list,
+        addToList
+    };
+  return (
+    <div className="App">
+        <Examples {...exampleProps}>Example One</Examples>
+    </div>
+  );
+}
+
+export default App;
