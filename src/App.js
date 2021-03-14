e51ecbe4138158c5a48ad9a87c575e6edaebfb58
RenasSitdikov <renas.sitdikov@flatstack.com>
2021-03-14 17:14:16 +0300

    added husky

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import { useState } from 'react';
 import './App.css';
 import Examples from './examples';
 
-const initialList = [
-    { name: 'Скачать и попробовать установить ESLnt и Prettier' },
-];
+const initialList = [{ name: 'Скачать и попробовать установить ESLnt и Prettier' }];
 
 function App() {
-    const [list, setList] = useState(initialList);
-    const addToList = (newElement) => setList([ ...list, { name: newElement }]);
-    const exampleProps = {
-        size: 5,
-        error: false,
-        list,
-        addToList
-    };
+  const [list, setList] = useState(initialList);
+  const addToList = (newElement) => setList([...list, { name: newElement }]);
+  const exampleProps = {
+    size: 5,
+    error: false,
+    list,
+    addToList,
+  };
   return (
     <div className="App">
-        <Examples {...exampleProps}>Example One</Examples>
+      <Examples {...exampleProps}>Example One</Examples>
     </div>
   );
 }
