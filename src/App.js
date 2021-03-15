945c8ed49cb6a86734198ccfaf08fb4aac793311
RenasSitdikov <renas.sitdikov@flatstack.com>
2021-03-15 18:57:22 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import { useState } from 'react';
-import { ThemeProvider as StyledThemeProvider } from 'styled-components';
-import defaultTheme from './config/theme/default';
-import './App.css';
-import Examples from './examples';
+import ToDoPage from './components/pages/ToDoPage';
 
-const initialList = [{ name: 'Скачать и попробовать установить ESLnt и Prettier' }];
-
-function App() {
-  const [list, setList] = useState(initialList);
-  const addToList = (newElement) => setList([...list, { name: newElement }]);
-  const exampleProps = {
-    size: 5,
-    error: false,
-    list,
-    addToList,
-  };
-  return (
-    <StyledThemeProvider theme={defaultTheme}>
-      <div className="App">
-        <Examples {...exampleProps}>Example One</Examples>
-      </div>
-    </StyledThemeProvider>
-  );
-}
+const App = () => (
+  <>
+    <ToDoPage />
+  </>
+);
 
 export default App;
