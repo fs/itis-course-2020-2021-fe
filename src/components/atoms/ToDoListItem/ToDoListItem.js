60cf93751053fcfb1df83b43dc115ce94de3bbef
RenasSitdikov <renas.sitdikov@flatstack.com>
2021-03-29 18:56:19 +0300

    lession complete

--- src/components/atoms/ToDoListItem/ToDoListItem.js
+++ src/components/atoms/ToDoListItem/ToDoListItem.js
@@ -1,15 +1,21 @@
 import styled from 'styled-components';
 
-const StyledListItem = styled.li`
+const StyledListItem = styled.li(
+  ({ checked }) => `
   min-width: 300px;
   display: flex;
   align-items: center;
   margin-bottom: 8px;
-`;
+  span {
+    text-decoration: ${checked ? 'line-through' : 'none'};
+    color: ${checked ? 'grey' : 'inherit'};
+  }
+`,
+);
 
 const ToDoListItem = (props) => {
-  const { children } = props;
-  return <StyledListItem>{children}</StyledListItem>;
+  const { children, checked } = props;
+  return <StyledListItem checked={checked}>{children}</StyledListItem>;
 };
 
 export default ToDoListItem;
