8b81574c46c93b6fccfbb429fdb0ba7c1283d9b9
RenasSitdikov <renas.sitdikov@flatstack.com>
2021-05-05 23:16:05 +0300

    fixed tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Example One/i);
+  const linkElement = screen.getByText('See tasks with GraphQL');
   expect(linkElement).toBeInTheDocument();
 });
