5149e7b66a510305954c4cbe39c2f7100b08e388
RenasSitdikov <renas.sitdikov@flatstack.com>
2021-05-06 01:08:22 +0300

    jest works

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,6 @@
 // fix https://github.com/testing-library/dom-testing-library/releases/tag/v7.0.0
 import MutationObserver from '@sheerun/mutationobserver-shim';
+import '@testing-library/jest-dom';
 
 window.MutationObserver = MutationObserver;
 
